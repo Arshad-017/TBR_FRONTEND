c7da7a3625b0ba602eedfba7e7b5ddd44eb4dae0
Arshad-017 <arshmujawar17@gmail.com>
2023-11-22 20:59:38 +0530

    Contact Validations Added

--- src/app/Component/about/about.component.ts
+++ src/app/Component/about/about.component.ts
@@ -12,16 +12,31 @@ export class AboutComponent implements OnInit {
   notifyForm!:FormGroup;
   submitted:boolean=false;
   respMessage!:string;
+  allFiledsRequired:boolean = false;
   constructor(private fb : FormBuilder,
     private notifyus : ContactusService){
 
   }
   notify(){
-    this.submitted=true;
-    this.notifyus.notifyus(this.notifyForm.value).subscribe(data=>{
+    this.allFiledsRequired = false;
+    if(this.Authenticate()){
+      this.submitted=true;
+      this.notifyus.notifyus(this.notifyForm.value).subscribe(data=>{
       this.respMessage=data.message;
     })
+    }
+    else{
+      this.allFiledsRequired = true;
+    }
     
+  }
+  Authenticate(){
+    if(this.notifyForm.get('fullname')?.value.length==0 || this.notifyForm.get('email')?.value.length==0 ||
+       this.notifyForm.get('subject')?.value.length==0 || this.notifyForm.get('message')?.value.length==0)
+    {
+      return false;
+    }
+    return true;
   }
   ngOnInit() {
       this.notifyForm = this.fb.group({
--- src/app/Component/contact/contact.component.ts
+++ src/app/Component/contact/contact.component.ts
@@ -5,61 +5,53 @@ import { ContactusService } from 'src/app/Service/contactus.service';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-   
-  contactForm1!:FormGroup;
-  userLoggedIn!:any;
-  isSubmitted:boolean=false;
-  respMessage!:string;
-  MessageMandantory:boolean=false;
-  emailMandantory:boolean=false;
-  nameMandantory:boolean=false;
-  constructor(private fb : FormBuilder,
-    private contactUs : ContactusService){
-
-  }
-   ngOnInit() {
-      this.userLoggedIn = sessionStorage.getItem("userLoginDetails");
-      this.userLoggedIn = JSON.parse(this.userLoggedIn);
-      this.contactForm1= this.fb.group({
-              fullname : [this.userLoggedIn.fullName , Validators.required],
-              email : [this.userLoggedIn.emailId , Validators.required],
-              message : ['' , Validators.required] ,
-          })
-   }
-
-
-   Authenticate():boolean{
-       this.MessageMandantory=false;
-       this.emailMandantory=false;
-       this.nameMandantory=false;
-       if(this.contactForm1.get('message')?.value.length > 0){
-           if(this.contactForm1.get('fullname')?.value.length > 0){
-               if(this.contactForm1.get('email')?.value.length > 0){
-                  return true;
-               }
-               else{
-                this.emailMandantory=true;
-               }
-           }
-           else{
-            this.nameMandantory=true;
-           }
-       }else{
-        this.MessageMandantory=true;
-       }
-       return false;
-   }
-  callContactHelp(){
-    if(this.Authenticate())
-    console.log(this.contactForm1.value)
-    this.contactUs.contactUs(this.contactForm1.value).subscribe(data=>{
-      this.respMessage = data.message;
-      this.isSubmitted=true;
-    })
-       
+  contactForm1!: FormGroup;
+  userLoggedIn!: any;
+  isSubmitted: boolean = false;
+  respMessage!: string;
+  MessageMandantory: boolean = false;
+  emailMandantory: boolean = false;
+  nameMandantory: boolean = false;
+  constructor(private fb: FormBuilder, private contactUs: ContactusService) {}
+  ngOnInit() {
+    this.userLoggedIn = sessionStorage.getItem('userLoginDetails');
+    this.userLoggedIn = JSON.parse(this.userLoggedIn);
+    this.contactForm1 = this.fb.group({
+      fullname: [this.userLoggedIn.fullName, Validators.required],
+      email: [this.userLoggedIn.emailId, Validators.required],
+      message: ['', Validators.required],
+    });
   }
 
+  Authenticate(): boolean {
+    this.MessageMandantory = false;
+    this.emailMandantory = false;
+    this.nameMandantory = false;
+    if (this.contactForm1.get('message')?.value.length > 0) {
+      if (this.contactForm1.get('fullname')?.value.length > 0) {
+        if (this.contactForm1.get('email')?.value.length > 0) {
+          return true;
+        } else {
+          this.emailMandantory = true;
+        }
+      } else {
+        this.nameMandantory = true;
+      }
+    } else {
+      this.MessageMandantory = true;
+    }
+    return false;
+  }
+  callContactHelp() {
+    if (this.Authenticate()) {
+      console.log(this.contactForm1.value);
+      this.contactUs.contactUs(this.contactForm1.value).subscribe((data) => {
+        this.respMessage = data.message;
+        this.isSubmitted = true;
+      });
+    }
+  }
 }
